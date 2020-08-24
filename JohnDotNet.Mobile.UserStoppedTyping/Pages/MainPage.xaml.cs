ae5c90300fae470f611983775ea5abfff51dd627
John Bijsterbosch - John.NET Development <john.bijsterbosch@live.com>
2020-08-24 20:31:12 +0200

    Implemented test page for UserStoppedTypingBehavior

---
+++ JohnDotNet.Mobile.UserStoppedTyping/Pages/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using JohnDotNet.Mobile.UserStoppedTyping.ViewModels;
+using Xamarin.Forms;
+
+namespace JohnDotNet.Mobile.UserStoppedTyping.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BindingContext = new MainPageViewModel();
+            
+            InitializeComponent();
+        }
+    }
+}
---
+++ JohnDotNet.Mobile.UserStoppedTyping/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace JohnDotNet.Mobile.UserStoppedTyping.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        #region Properties
+        
+        private string _performedSearches;
+        public string PerformedSearches
+        {
+            get => _performedSearches;
+            set => SetProperty(ref _performedSearches, value);
+        }
+
+        public ICommand SearchCommand { get; }
+
+        #endregion Properties
+
+        public MainPageViewModel()
+        {
+            SearchCommand = new Command<string>(PerformSearch);
+        }
+
+        private void PerformSearch(string searchTerms)
+        {
+            PerformedSearches += $"Performed search for '{searchTerms}'{Environment.NewLine}";
+        }
+    }
+}
